b9b2c39932c3395ff54fc6a02ead08500dcd903c
John Patrick Dandison <johndand@microsoft.com>
2018-01-09 17:14:35 -0500

    removed -Force from Set-PSRepository

--- dtl-artifacts/windows-azurepowershell/install-azps.ps1
+++ dtl-artifacts/windows-azurepowershell/install-azps.ps1
@@ -1,4 +1,4 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Register-PSRepository -Default -InstallationPolicy Trusted -ErrorAction SilentlyContinue
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module Azure -Force
